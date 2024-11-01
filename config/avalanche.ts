9091244ca38d23bc831aa2d5763be3a2415ff104
raznorabochiy <0xe8eabec7ce9e8bf78a766e8556e542bc2c9446ae@dmail.ai>
2024-11-01 16:09:23 +0300

    Remove whitespace

--- config/avalanche.ts
+++ config/avalanche.ts
@@ -1,4 +1,4 @@
-export const RPC_URL = "https://ava-mainnet.public.blastapi.io/ext/bc/C/rpc	";
+export const RPC_URL = "https://ava-mainnet.public.blastapi.io/ext/bc/C/rpc";
 
 const USDC = "0xb97ef9ef8734c71904d8002f8b6bc66dd9c48a6e";
 
